66986c8395d80f75666b47699511ae6dba3b5058
kristivent <ventk84@gmail.com>
2025-02-25 21:48:33 -0500

    reorganized server folders, added models, schema, utils, and updated seeds

--- server/src/models/Park.ts
+++ server/src/models/Park.ts
@@ -1,1 +1,36 @@
-//Park model
+import { Schema, model, Document } from 'mongoose';
+
+interface IPark extends Document {
+    name: string;
+    species: string[];
+    code: string;
+}
+
+const parkSchema = new Schema<IPark>(
+     {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    species: {
+      type: [String],
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Create and export the Park model
+const Park = model<IPark>('Park', parkSchema);
+
+export default Park;
---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,86 @@
+import { Park, User } from '../models/index.js';
+import { signToken, AuthenticationError } from '../utils/auth.js';
+
+// Define types for the arguments
+interface AddUserArgs {
+  input: {
+    username: string;
+    email: string;
+    password: string;
+  };
+}
+
+interface LoginUserArgs {
+  email: string;
+  password: string;
+}
+
+interface SaveFavSpeciesArgs {
+  species: string;
+}
+
+interface DeleteFavSpeciesArgs {
+  species: string;
+}
+
+const resolvers = {
+  Query: {
+    me: async (_parent: any, _args: any, context: any) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('thoughts');
+      }
+      throw new AuthenticationError('Could not authenticate user.');
+    },
+    getParksByName: async (_parent: any, { name }: { name: string }) => {
+      return Park.find({ name: new RegExp(name, 'i') });
+    },
+    getParkByCode: async (_parent: any, { code }: { code: string }) => {
+      return Park.findOne({ code });
+    },
+  },
+
+  Mutation: {
+    addUser: async (_parent: any, { input }: AddUserArgs) => {
+      const user = await User.create(input);
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+
+    login: async (_parent: any, { email, password }: LoginUserArgs) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+
+    saveFavSpecies: async (_parent: any, { species }: SaveFavSpeciesArgs, context: any) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { favoriteSpecies: species } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    deleteFavSpecies: async (_parent: any, { species }: DeleteFavSpeciesArgs, context: any) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { favoriteSpecies: species } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+};
+
+export default resolvers;
